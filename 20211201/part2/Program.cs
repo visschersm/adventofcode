8eac7fdf7e1c31554dc944ae59ffb201f207ca5d
Mark Visschers <visschers.m@gmail.com>
2021-12-06 08:55:14 +0100

    first commit

---
+++ 20211201/part2/Program.cs
@@ -0,0 +1,17 @@
+﻿var measurements = File.ReadAllLines("input.txt").Select(x => int.Parse(x));
+var increasedCounter = 0;
+
+var previousMeasurement = measurements.Skip(0).Take(3).Sum();
+Console.WriteLine($"{previousMeasurement} (N/A - no previous measurement)");
+
+for (int i = 1; i < measurements.Count() - 2; ++i)
+{
+    var meassurementWindow = measurements.Skip(i).Take(3);
+    var meassurement = meassurementWindow.Sum();
+    var increased = meassurement > previousMeasurement;
+    Console.WriteLine($"{string.Join(", ", meassurementWindow)} - {meassurement} ({(increased ? "increased" : "decreased")})");
+    if (increased) increasedCounter++;
+    previousMeasurement = meassurement;
+}
+
+Console.WriteLine($"increasedCounter: {increasedCounter}");
---
+++ 20211203/part1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+
+Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllLines("input.txt");
+var charArrays = input.Select(x => x.ToCharArray()).ToArray();
+var inputCount = charArrays.Count();
+
+Console.WriteLine($"InputCount: {inputCount}");
+
+var gammaRate = new List<char>();
+var epsilonRate = new List<char>();
+
+for (int i = 0; i < charArrays.First().Count(); ++i)
+{
+    var column = charArrays.Select(x => int.Parse(x[i].ToString()));
+    var summedValue = column.Sum(x => x);
+
+    if (summedValue >= (double)inputCount / 2)
+    {
+        gammaRate.Add('1');
+        epsilonRate.Add('0');
+    }
+    else
+    {
+        epsilonRate.Add('1');
+        gammaRate.Add('0');
+    }
+
+
+    Console.WriteLine($"Column: {i} ({string.Join("", column)})\tSum: {summedValue}\t{summedValue}|{(double)inputCount / 2}");
+}
+
+var gammaValue = 0;
+var epsilonValue = 0;
+
+gammaValue = BinaryToDec(new string(gammaRate!.ToArray()));
+epsilonValue = BinaryToDec(new string(epsilonRate!.ToArray()));
+
+Console.WriteLine($"Gamma: {string.Join("", gammaRate)} ({gammaValue})");
+Console.WriteLine($"Epsilon: {string.Join("", epsilonRate)} ({epsilonValue})");
+
+Console.WriteLine($"Powerlevel: {gammaValue * epsilonValue}");
+
+static int BinaryToDec(string input)
+{
+    Console.WriteLine($"BinaryToDec: {input}");
+    char[] array = input.ToCharArray();
+    // Reverse since 16-8-4-2-1 not 1-2-4-8-16. 
+    Array.Reverse(array);
+    /*
+     * [0] = 1
+     * [1] = 2
+     * [2] = 4
+     * etc
+     */
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == '1')
+        {
+            sum += (int)Math.Pow(2, i);
+        }
+
+    }
+
+    return sum;
+}
