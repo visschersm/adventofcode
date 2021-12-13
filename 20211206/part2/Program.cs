5154c43fdb4212777f0f448fab99e63dc1c60f2e
Mark Visschers <visschers.m@gmail.com>
2021-12-13 09:06:49 +0100

    Started day 7

--- 20211206/part2/Program.cs
+++ 20211206/part2/Program.cs
@@ -1,19 +1,47 @@
-﻿var input = File.ReadAllLines("test-input.txt")
+﻿var input = File.ReadAllLines("input.txt")
     .First()
     .Split(",")
     .Select(x => int.Parse(x))
+    .GroupBy(x => x)
+    .Select(x => new Counted { Key = x.Key, Count = x.Count()})
     .ToList();
 
 var endOfTime = 256;
 
+
+Console.WriteLine($"Initial State:\t{string.Join(",", input)}");
 for(int i = 1; i <= endOfTime; ++i)
 {
-    input = input.Select(x => x - 1).ToList();
-    var newGenerationCount = input.Count(x => x == -1);
-    input = input.Select(x => x == -1 ? 6 : x).ToList();
-    input.AddRange(Enumerable.Repeat(8, newGenerationCount));
+    input = input.Select(x => new Counted
+    {
+        Key = x.Key - 1,
+        Count = x.Count
+    }).ToList();
 
-    Console.WriteLine($"After {i} days:\t{input.Count()}");
+    var newGenerationCount = input.SingleOrDefault(x => x.Key == -1)?.Count ?? 0;
+
+    var foo = input.SingleOrDefault(x => x.Key == 6);
+    if(foo == null)
+        input.Add(new Counted { Key = 6, Count = 0 });
+
+    input.Single(x => x.Key == 6).Count += input.SingleOrDefault(x => x.Key == -1)?.Count ?? 0;
+    input = input.Where(x => x.Key != -1).ToList();
+
+    input.Add(new Counted { Key = 8, Count = newGenerationCount });
+
+    //Console.WriteLine($"After {i} days:\t{string.Join(",", input)}");
+    //Console.WriteLine($"After {i} days:\t{input.Sum(x => x.Count)}");
 }
 
-Console.WriteLine($"Count: {input.Count()}");
+
+Console.WriteLine($"Count: {input.Sum(x => x.Count)}");
+
+public class Counted
+{
+    public int Key;
+    public long Count;
+    public override string ToString()
+    {
+        return $"{Key}|{Count}";
+    }
+}
---
+++ 20211207/part1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Linq;
+
+var input = File.ReadAllLines("input.txt")
+    .First()
+    .Split(",")
+    .Select(x => int.Parse(x));
+
+var min = input.Min();
+var max = input.Max();
+
+int lowestFuelConsumption = int.MaxValue;
+for(int i = min; i < max; ++i)
+{
+    lowestFuelConsumption = Math.Min(lowestFuelConsumption, input.Select(x => Math.Abs(x - i)).Sum());
+}
+
+Console.WriteLine($"LowestFuelConsumption: {lowestFuelConsumption}");
