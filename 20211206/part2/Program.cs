7f2731ed3a7d6f05ca068012875a5a12145a2da2
Mark Visschers <visschers.m@gmail.com>
2021-12-13 01:39:12 +0100

    Is this quick enough?

---
+++ 20211206/part2/Program.cs
@@ -0,0 +1,19 @@
+﻿var input = File.ReadAllLines("test-input.txt")
+    .First()
+    .Split(",")
+    .Select(x => int.Parse(x))
+    .ToList();
+
+var endOfTime = 256;
+
+for(int i = 1; i <= endOfTime; ++i)
+{
+    input = input.Select(x => x - 1).ToList();
+    var newGenerationCount = input.Count(x => x == -1);
+    input = input.Select(x => x == -1 ? 6 : x).ToList();
+    input.AddRange(Enumerable.Repeat(8, newGenerationCount));
+
+    Console.WriteLine($"After {i} days:\t{input.Count()}");
+}
+
+Console.WriteLine($"Count: {input.Count()}");
