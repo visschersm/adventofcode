c0efb7ed709f5580cfd9f8506db4505773544f18
Mark Visschers <visschers.m@gmail.com>
2022-10-06 17:11:31 +0700

    csharp solution 2015/01

---
+++ 2015/01/csharp/part1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.IO;
+
+var filename = "input.txt";
+var data = File.ReadAllText(filename).Select(c => c == '(' ? 1 : -1);
+
+Console.WriteLine("Santa is on the {0}th floor", data.Sum());
