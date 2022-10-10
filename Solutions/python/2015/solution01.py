da5308a2ae7cdd4b0a8df8c2d27cabc2deb58303
Mark Visschers <visschers.m@gmail.com>
2022-10-11 00:35:20 +0700

    bit of cleanup

--- Solutions/python/2015/solution01.py
+++ Solutions/python/2015/solution01.py
@@ -1,34 +1,35 @@
 import sys
 
-def part1(filename):
+def part1(input_file):
     floor = 0
-
-    with open(filename, 'r') as input_file:
-        for i, line in enumerate(input_file):
-            for char in line:
-                print("Char found:", char)
-                print("Current floor:", floor)
-                floor = floor + 1 if char == '(' else floor - 1
+    for c in read_by_character(input_file):
+        floor += 1 if c == '(' else -1
 
     print("Santa is on the %dth floor" % floor)
 
-def part2(filename):
+def part2(input_file):
     floor = 0
     charCounter = 0
 
-    with open(filename, 'r') as input_file:
-        for i, line in enumerate(input_file):
-            for char in line:
-                charCounter += 1
-                floor = floor + 1 if char == '(' else floor - 1
-                if floor == -1:
-                    print("Santa found the basement after %d tries" % charCounter)
-                    break
+    for c in read_by_character(input_file):
+        charCounter += 1
+        floor += 1 if c == '(' else - 1
+        if floor == -1:
+            break
+    
+    print("Santa found the basement after %d tries" % charCounter)
+
+def read_by_character(input_file):
+    for i, line in enumerate(input_file):
+        for char in line:
+            yield char
 
 def solve(filename):
-    print("Part1:")
-    part1(filename)
+    with open(filename, 'r') as input_file:
+        print("Part1:")
+        part1(input_file)
     
-    print("Part2:")
-    part2(filename)
+        input_file.seek(0,0)
+        print("Part2:")
+        part2(input_file)
 
