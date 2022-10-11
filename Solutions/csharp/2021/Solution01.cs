889036dec1b587584b7772546abf7eace98377ca
Mark Visschers <visschers.m@gmail.com>
2022-10-11 22:21:49 +0700

    Refactor of 2021/Solution01 part2 in csharp

--- Solutions/csharp/2021/Solution01.cs
+++ Solutions/csharp/2021/Solution01.cs
@@ -24,20 +24,17 @@ public class Solution01
     [Part2]
     public void Part2(string filename)
     {
-        var measurements = File.ReadAllLines(filename).Select(x => int.Parse(x));
-        var increasedCounter = 0;
+        var measurements = File.ReadAllLines(filename).Select(x => int.Parse(x)).ToArray();
 
-        var previousMeasurement = measurements.Skip(0).Take(3).Sum();
-
-        for (int i = 1; i < measurements.Count() - 2; ++i)
+        var result = Enumerable.Range(1, measurements.Count() - 3).Aggregate(0, (result, index) =>
         {
-            var meassurementWindow = measurements.Skip(i).Take(3);
-            var meassurement = meassurementWindow.Sum();
-            var increased = meassurement > previousMeasurement;
-            if (increased) increasedCounter++;
-            previousMeasurement = meassurement;
-        }
-
-        Console.WriteLine($"increasedCounter: {increasedCounter}");
+            int current = measurements[index - 1] + measurements[index] + measurements[index + 1];
+            int next = measurements[index] + measurements[index + 1] + measurements[index + 2];
+            
+            result += current < next ? 1 : 0;
+            return result;
+        });
+        
+        Console.WriteLine($"increasedCounter: {result}");
     }
 }
