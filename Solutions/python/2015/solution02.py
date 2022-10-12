c5407eb78cf45f34bd66b9e0af0883a9b60c7ad1
Mark Visschers <visschers.m@gmail.com>
2022-10-12 11:10:45 +0700

    Finished 2015 solution02 part2 in python

--- Solutions/python/2015/solution02.py
+++ Solutions/python/2015/solution02.py
@@ -1,6 +1,8 @@
 from cgitb import small
 from mymath.geometry import cube
 from util.filereader import *
+from operator import mul
+from functools import reduce
 
 
 def part1(input_file):
@@ -9,13 +11,19 @@ def part1(input_file):
     for line in read_by_line(input_file):
         dimensions = [int(numeric_string) for numeric_string in str.split(line, "x")]
         gift_surface = cube.surface(dimensions[0], dimensions[1], dimensions[2])
-        #print("gift surface:", gift_surface)
         smallest_surface = cube.smallest_surface(dimensions[0], dimensions[1], dimensions[2])
-        #print("smallest surface", smallest_surface)
         total += gift_surface + smallest_surface
     
     print("The elves need to order", total, "square feet of wrapping paper") 
 
 def part2(input_file):
-    print("Part2 not yet done")
+    total = 0
+
+    for line in read_by_line(input_file):
+        dimensions = [int(numeric_string) for numeric_string in str.split(line, "x")]
+        sorted_dimensions = sorted(dimensions)[0:2]
+        total += sorted_dimensions[0] * 2 + sorted_dimensions[1] * 2 + cube.cubed(dimensions[0], dimensions[1], dimensions[2])
+
+    print("The elves need to order", total, "feet of ribbon.");
+
 
