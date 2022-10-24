60a56f15ac161203b81cb5196164ba41a1947ffd
Mark Visschers <visschers.m@gmail.com>
2022-10-24 23:07:12 +0700

    Current kotlin state

--- Solutions/kotlin/adventofcode.kt
+++ Solutions/kotlin/adventofcode.kt
@@ -1,22 +1,22 @@
-import Date
-import Solutions.*
+// import Solutions.*
 
 fun main(args: Array<String>) {
     val dateStr = args[0]
     val date = getDate(dateStr)
-    val solution = getSolution(date)
-    solve(solution)
+    print(date.Format())
+    // val solution = getSolution(date)
+    // solve(solution)
 }
 
 fun getDate(date: String): Date {
     return Date(2015, 1);
 }
 
-fun getSolution(date: Date): Solution {
-    return Solution01();
-}
+// fun getSolution(date: Date): Solution {
+//     return Solution01();
+// }
 
-fun solve(solution: Solution) {
-    solution.Part1("hello world");
-    solution.Part2("hello world");
-}
+// fun solve(solution: Solution) {
+//     solution.Part1("hello world");
+//     solution.Part2("hello world");
+// }
