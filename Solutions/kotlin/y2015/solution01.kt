4627c9daee253bd122a44c467bcb2d7efe4ea583
Mark Visschers <visschers.m@gmail.com>
2022-10-25 23:00:46 +0700

    Implemented solution 2015/01 in kotlin

--- Solutions/kotlin/y2015/solution01.kt
+++ Solutions/kotlin/y2015/solution01.kt
@@ -1,14 +1,37 @@
 package Solutions.kotlin.y2015
 
 import Solutions.kotlin.Solution
+import java.io.InputStream
+import java.io.File
+import java.io.BufferedReader
 
 class Solution01 : Solution
 {
     override fun Part1(inputFile: String) {
-        print("Part1 not yet implemented\n");
+        val data = File(inputFile).readText()
+        var result = data.count { it == '(' } - data.count { it == ')' }
+        
+
+        print("Santa is on the %dth floor\n".format(result));
     }
     
     override fun Part2(inputFile: String) {
-        print("Part2 not yet implemented\n");
+        var result = 0
+        var tryCounter = 0
+
+        File(inputFile).useLines() { 
+            lines -> lines.forEach { 
+                for (c in it) {
+                    result += if(c == '(') 1 else -1
+                    tryCounter++
+
+                    if(result < 0) {
+                        break
+                    }
+                } 
+            } 
+        }
+
+        print("Santa found the basement after %d tries\n".format(tryCounter));
     }
 }
