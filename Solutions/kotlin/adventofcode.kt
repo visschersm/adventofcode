66115194ad4f5c02a5a7bec5791d838f32c5280b
Mark Visschers <visschers.m@gmail.com>
2022-10-25 17:05:11 +0700

    Fixed running of solution for kotlin

--- Solutions/kotlin/adventofcode.kt
+++ Solutions/kotlin/adventofcode.kt
@@ -1,5 +1,7 @@
 package Solutions.kotlin;
 
+import kotlin.reflect.cast
+
 fun main(args: Array<String>) {
     if (args.size == 0) {
         println("Please provide a date for which to solve a challenge")
@@ -13,9 +15,15 @@ fun main(args: Array<String>) {
 }
 
 fun getDate(date: String): Date {
-    return Date(2015, 1);
+    val splittedDate = date.split("/")
+    return Date(splittedDate[0].toInt(), splittedDate[1].toInt())
 }
 
 fun solve(date: Date) {
-    print("Solve for %d/%02d\n".format(date.year, date.day))
+    val className = "Solutions.kotlin.y%d.Solution%02d".format(date.year, date.day)
+    val classObject = Class.forName(className).kotlin
+    val ctor = classObject.constructors.first()
+    val obj = ctor.call() as Solution
+    obj.Part1("inputfile")
+    obj.Part2("inputfile")
 }
