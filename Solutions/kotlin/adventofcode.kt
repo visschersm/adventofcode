cf3548c8897f6948ab3adc7a2887750a1c77be29
Mark Visschers <visschers.m@gmail.com>
2022-10-25 15:18:30 +0700

    Working on kotlin adventofcode solver

--- Solutions/kotlin/adventofcode.kt
+++ Solutions/kotlin/adventofcode.kt
@@ -1,7 +1,5 @@
 package Solutions.kotlin;
 
-import Solutions.kotlin.Date;
-
 fun main(args: Array<String>) {
     if (args.size == 0) {
         println("Please provide a date for which to solve a challenge")
@@ -10,20 +8,14 @@ fun main(args: Array<String>) {
 
     val dateStr = args[0]
     val date = getDate(dateStr)
-    print(date.Format())
-    // val solution = getSolution(date)
-    // solve(solution)
+    
+    solve(date)
 }
 
 fun getDate(date: String): Date {
     return Date(2015, 1);
 }
 
-// fun getSolution(date: Date): Solution {
-//     return Solution01();
-// }
-
-// fun solve(solution: Solution) {
-//     solution.Part1("hello world");
-//     solution.Part2("hello world");
-// }
+fun solve(date: Date) {
+    print("Solve for %d/%02d\n".format(date.year, date.day))
+}
