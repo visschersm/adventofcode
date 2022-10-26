6684f5da80ced64c76a558f18841ddb64219585e
Mark Visschers <visschers.m@gmail.com>
2022-10-26 23:50:07 +0700

    go_runner now generates registration file

--- cli/runners/go_runner.go
+++ cli/runners/go_runner.go
@@ -12,14 +12,29 @@ type GoRunner struct{}
 func (runner *GoRunner) Run(date util.Date, input_file string) {
 	cmd := exec.Command(
 		"go",
+		"generate",
+		`.\Solutions\go\adventofcode.go`)
+
+	executeCommand(cmd, false)
+
+	cmd = exec.Command(
+		"go",
 		"run",
-		`.\Solutions\go\main.go`,
+		`.\Solutions\go\adventofcode.go`,
 		"-date",
 		date.Format())
+
+	executeCommand(cmd, true)
+}
+
+func executeCommand(cmd *exec.Cmd, print bool) {
 	stdout, err := cmd.Output()
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(stdout))
+
+	if print {
+		fmt.Println(string(stdout))
+	}
 }
