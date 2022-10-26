ad44d72dffaf79954d7873f6b5d6b0048ba8931c
Mark Visschers <visschers.m@gmail.com>
2022-10-27 00:10:15 +0700

    Solved challenge 2015/01 in vlang

--- Solutions/v/y2015/Solution01.v
+++ Solutions/v/y2015/Solution01.v
@@ -1,13 +1,42 @@
 module y2015
 
+import os
+
 pub struct Solution01 {
 
 }
 
 pub fn (s Solution01) part1(input_file string) {
-	println("Part1 not yet implemented")
+	data := os.read_file(input_file) or {
+		println(err)
+		return
+	}
+
+	mut result := 0
+	for c in data {
+		result += if c == '('.bytes()[0] { 1 } else { -1 }
+	}
+
+	println("Santa is on the ${result}th floor")
 }
 
 pub fn (s Solution01) part2(input_file string) {
-	println("Part2 not yet implemented")
+	data := os.read_file(input_file) or {
+		println(err)
+		return
+	}
+
+	mut result := 0
+	mut try_counter := 0
+
+	for c in data {
+		result += if c == '('.bytes()[0] { 1 } else { -1 }
+		try_counter++
+
+		if result < 0 {
+			break
+		}
+	}
+
+	println("Santa found the basement after $try_counter tries")
 }
