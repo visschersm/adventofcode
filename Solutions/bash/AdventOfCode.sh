204d130b16efb547b13fb66bdcc34f021c4caae6
Mark Visschers <visschers.m@gmail.com>
2022-10-30 10:55:50 +0700

    Added input file and date checking

--- Solutions/bash/AdventOfCode.sh
+++ Solutions/bash/AdventOfCode.sh
@@ -8,12 +8,16 @@ function usage {
 }
 
 declare DATE
+declare INPUT_FILE
 
-optstring="d:"
+optstring="d:i:"
 while getopts ${optstring} arg; do
     case ${arg} in
         d)
             DATE="${OPTARG}"
+            ;;
+        i)
+            INPUT_FILE="${OPTARG}"
             ;;
         ?)
             echo "Invalid option: -${OPTARG}."
@@ -22,6 +26,11 @@ while getopts ${optstring} arg; do
             ;;
     esac
 done
+
+if test -z "$DATE"; then
+    echo "Provide a date for which to solve a challenge."
+    usage
+fi
 
 function get_solution() {
     local date=$1
@@ -32,8 +41,21 @@ function get_solution() {
     echo "Solutions/bash/y$YEAR/Solution`printf %02d $DAY`.sh"
 }
 
+function get_inputfile() {
+    local date=$1
+    IFS="/"
+    read -ra newarr <<< "$DATE"
+    YEAR=${newarr[0]}
+    DAY=${newarr[1]}
+    echo "Inputs/$YEAR/`printf %02d $DAY`.txt"
+}
+
 SOLUTION=$(get_solution $DATE)
 
+if test -z "$INPUT_FILE"; then
+    INPUT_FILE=$(get_inputfile $DATE)
+fi
+
 source "./$SOLUTION"
-Part1
-Part2
+Part1 $INPUT_FILE
+Part2 $INPUT_FILE
