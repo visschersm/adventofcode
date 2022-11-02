564755e93b180d8cf032fcd48f1a4c3727b6b9f1
Mark Visschers <visschers.m@gmail.com>
2022-11-03 00:33:25 +0800

    New languages module with languages configured internal

---
+++ cli/util/Languages/language.go
@@ -0,0 +1,77 @@
+package languages
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+type Language struct {
+	Name     string
+	Ext      string
+	Commands []Command
+}
+
+func GetAvailableLanguages() []string {
+	var result []string
+
+	folders, err := os.ReadDir("Solutions")
+
+	if err != nil {
+		log.Fatal(err)
+		return result
+	}
+
+	for _, folder := range folders {
+		result = append(result, folder.Name())
+	}
+
+	return result
+}
+
+func GetRandomLanguage() Language {
+	availableLanguages := GetAvailableLanguages()
+	rand.Seed(time.Now().UnixNano())
+	min := 1
+	max := len(availableLanguages)
+	randomValue := rand.Intn(max-min) + min
+	randomLanguage := availableLanguages[randomValue-1]
+	language, err := GetLanguage(randomLanguage)
+	if err != nil {
+		fmt.Printf("An available language was not supported: %s\n", randomLanguage)
+	}
+	return *language
+}
+
+func GetRegisteredLanguages() []Language {
+	result := make([]Language, 0, len(Languages))
+
+	for _, value := range Languages {
+		result = append(result, value)
+	}
+
+	return result
+}
+
+func GetRegisteredLanguageNames() []string {
+	var supportedLanguages = GetRegisteredLanguages()
+	var result = make([]string, len(supportedLanguages))
+
+	for index, element := range supportedLanguages {
+		result[index] = element.Name
+	}
+
+	return result
+}
+
+func GetLanguage(languageName string) (*Language, error) {
+	language, exists := Languages[languageName]
+
+	if !exists {
+		return nil, fmt.Errorf("language not added to converter: %s", languageName)
+	}
+
+	return &language, nil
+}
---
+++ cli/util/Languages/languages.go
@@ -0,0 +1,330 @@
+package languages
+
+import (
+	"os/exec"
+)
+
+var Languages = map[string]Language{
+	"bash": {
+		Name: "bash",
+		Ext:  ".sh",
+		Commands: []Command{
+			{
+				Print: true,
+				Command: exec.Command(
+					"bash",
+					"Solutions/bash/AdventOfCode.sh",
+					"-d",
+					"<date>"),
+			},
+		},
+	},
+	"c": {
+		Name: "c",
+		Ext:  ".c",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "c not yet implemented"),
+			},
+		},
+	},
+	"carbon": {
+		Name: "carbon",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "carbon not yet implemented"),
+			},
+		},
+	},
+	"clojure": {
+		Name: "clojure",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "clojure not yet implemented"),
+			},
+		},
+	},
+	"cpp": {
+		Name: "cpp",
+		Ext:  ".cpp",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "cpp not yet implemented"),
+			},
+		},
+	},
+	"csharp": {
+		Name: "csharp",
+		Ext:  ".cs",
+		Commands: []Command{
+			{
+				Print: true,
+				Command: exec.Command(
+					"dotnet",
+					"run",
+					"--project",
+					"Solutions/csharp/adventofcode.csproj",
+					"<date>"),
+			},
+		},
+	},
+	"dart": {
+		Name: "dart",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "dart not yet implemented"),
+			},
+		},
+	},
+	"fsharp": {
+		Name: "fsharp",
+		Ext:  ".fs",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "fsharp not yet implemented"),
+			},
+		},
+	},
+	"go": {
+		Name: "go",
+		Ext:  ".go",
+		Commands: []Command{
+			{
+				Print:   false,
+				Command: exec.Command("go", "generate", `.\Solutions\go\adventofcode.go`),
+			},
+			{
+				Print: true,
+				Command: exec.Command("go",
+					"run",
+					`.\Solutions\go\adventofcode.go`,
+					"-date",
+					"<date>"),
+			},
+		},
+	},
+	"haskell": {
+		Name: "haskell",
+		Ext:  ".hs",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "haskell not yet implemented"),
+			},
+		},
+	},
+	"java": {
+		Name: "java",
+		Ext:  ".java",
+		Commands: []Command{
+			{
+				Print:   false,
+				Command: exec.Command("CMD", "/C", `cli\runners\java_runner.bat`),
+			},
+			{
+				Print: true,
+				Command: exec.Command(
+					"java",
+					"-cp",
+					".",
+					"Solutions/java/AdventOfCode",
+					"<date>"),
+			},
+		},
+	},
+	"javascript": {
+		Name: "javascript",
+		Ext:  ".js",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "javascript not yet implemented"),
+			},
+		},
+	},
+	"kotlin": {
+		Name: "kotlin",
+		Ext:  ".kt",
+		Commands: []Command{
+			{
+				Print: false,
+				Command: exec.Command(
+					"kotlinc",
+					"-cp",
+					".",
+					"-d",
+					"Solutions/kotlin/build",
+					"Solutions/kotlin"),
+			},
+			{
+				Print: true,
+				Command: exec.Command(
+					"kotlin",
+					"-cp",
+					"Solutions/kotlin/build",
+					"Solutions.kotlin.AdventOfCodeKt",
+					"<date>"),
+			},
+		},
+	},
+	"lua": {
+		Name: "lua",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "lua not yet implemented"),
+			},
+		},
+	},
+	"matlab": {
+		Name: "matlab",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "matlab not yet implemented"),
+			},
+		},
+	},
+	"perl": {
+		Name: "perl",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "perl not yet implemented"),
+			},
+		},
+	},
+	"php": {
+		Name: "php",
+		Ext:  ".php",
+		Commands: []Command{
+			{
+				Print: true,
+				Command: exec.Command(
+					"php",
+					"Solutions/php/adventofcode.php",
+					"-d",
+					"<date>"),
+			},
+		},
+	},
+	"powershell": {
+		Name: "powershell",
+		Ext:  ".ps1",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "powershell not yet implemented"),
+			},
+		},
+	},
+	"python": {
+		Name: "python",
+		Ext:  ".py",
+		Commands: []Command{
+			{
+				Print: true,
+				Command: exec.Command(
+					"python",
+					`.\Solutions\python\adventofcode.py`,
+					"--date",
+					"<date>"),
+			},
+		},
+	},
+	"r": {
+		Name: "r",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "r not yet implemented"),
+			},
+		},
+	},
+	"ruby": {
+		Name: "ruby",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "ruby not yet implemented"),
+			},
+		},
+	},
+	"rust": {
+		Name: "rust",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "rust not yet implemented"),
+			},
+		},
+	},
+	"scala": {
+		Name: "scala",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "scala not yet implemented"),
+			},
+		},
+	},
+	"sql": {
+		Name: "sql",
+		Ext:  ".sql",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "sql not yet implemented"),
+			},
+		},
+	},
+	"swift": {
+		Name: "swift",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "cswiftnot yet implemented"),
+			},
+		},
+	},
+	"typescript": {
+		Name: "typescript",
+		Ext:  ".ts",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "typescript not yet implemented"),
+			},
+		},
+	},
+	"v": {
+		Name: "v",
+		Ext:  ".v",
+		Commands: []Command{
+			{
+				Print: true,
+				Command: exec.Command(
+					"v",
+					"run",
+					"Solutions/v",
+					"<date>"),
+			},
+		},
+	},
+	"vb": {
+		Name: "visualbasic",
+		Ext:  ".vb",
+		Commands: []Command{
+			{
+				Print:   true,
+				Command: exec.Command("echo", "visual basic not yet implemented"),
+			},
+		},
+	},
+}
