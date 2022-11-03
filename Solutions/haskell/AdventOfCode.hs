0d099283d16fbac7e72dd0e68dfdf3a6369bc5e7
Mark Visschers <visschers.m@gmail.com>
2022-11-03 10:25:42 +0800

    Working on haskell adventofcode

--- Solutions/haskell/AdventOfCode.hs
+++ Solutions/haskell/AdventOfCode.hs
@@ -1,18 +1,24 @@
+module Solutions.Haskell where
+
 import System.Environment   
 import Data.List
 import Data.List.Split
 
-import Y2015.Solution01
+import Solutions.Haskell.Y2015.Solution01
 
 main = do
     args <- getArgs
     let date = getDate args
     let solution = getSolutionName date
-    part1 "Hello, World"
-
+    let inputFile = getInputFile date
+    part1 inputFile
+    part2 inputFile
 
 getDate [] = error "Please provide a date for which to solve a challenge."
 getDate (x : xs) = splitOn "/" x
 
 getSolutionName [] = error "Please provide a date for which to solve a challenge."
 getSolutionName (x : xs) = "Solutions/haskell/y" ++ x ++ "/Solution" ++ (head xs) ++ ".hs"
+
+getInputFile [] = error "Please provide a date for which to solve a challenge."
+getInputFile (x : xs) = "Inputs/" ++ x ++ "/" ++ (head xs) ++ ".txt"
