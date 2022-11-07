df21e1d90faaa19f671333d0c6ddfead08a9036d
Mark Visschers <visschers.m@gmail.com>
2022-11-08 00:13:47 +0800

    Some code cleanup

--- Solutions/haskell/y2015/Solution02.hs
+++ Solutions/haskell/y2015/Solution02.hs
@@ -14,48 +14,27 @@ part1 :: String -> IO()
 part1 inputFile = do
     input <- readFile inputFile
     let linesOfFiles = lines input
-    let dimensions = get_dimensions linesOfFiles
-    let result = calculate_wrapping dimensions
+    let d = map dimensions linesOfFiles
+    let result = sum (map wrapping_paper d)
     print ("The elves need to order " ++ (show result) ++ " square feet of wrapping paper")
+
+wrapping_paper :: (Int, Int, Int) -> Int
+wrapping_paper (l, w, h) =
+    2 * sum sides + minimum sides 
+    where sides = [l * w, w * h, h * l]
 
 part2 :: String -> IO()
 part2 inputFile = do
     input <- readFile inputFile
     let linesOfFiles = lines input
-    let dimensions = get_dimensions linesOfFiles
-    let result = calculate_ribbon dimensions
+    let d = map dimensions linesOfFiles
+    let result = sum (map ribbon d)
     print ("The elves need to order " ++ (show result) ++ " feet of ribbon.")
 
-calculate_wrapping :: [[Int]] -> Int
-calculate_wrapping [] = 0
-calculate_wrapping (x : xs) = do
-    let l = x !! 0
-    let w = x !! 1
-    let h = x !! 2
-    (wrapping_paper l w h) + (calculate_wrapping xs)
+ribbon :: (Int, Int, Int) -> Int
+ribbon (l, w, h) = (2 * minimum[l + w, w + h, h + l]) + (l*w*h)
 
-calculate_ribbon :: [[Int]] -> Int
-calculate_ribbon [] = 0
-calculate_ribbon (x : xs) = do
-    let l = x !! 0
-    let w = x !! 1
-    let h = x !! 2
-    ((smallest_perimeter l w h) + (Math.cubed l w h)) + (calculate_ribbon xs)
-
-get_dimensions :: [String] -> [[Int]]
-get_dimensions[] = []
-get_dimensions (x : xs) =
-    map read (splitOn "x" x) : get_dimensions xs
-
-smallest_side :: Int -> Int -> Int -> Int
-smallest_side l w h =
-    Math.min (Math.min (l * w) (w * h)) (h * l)
-
-wrapping_paper :: Int -> Int -> Int -> Int
-wrapping_paper l w h =
-    (Math.surface l w h) + (smallest_side l w h)
-
-smallest_perimeter :: Int -> Int -> Int -> Int
-smallest_perimeter l w h = do
-    let sortedList = sort [l, w, h]
-    (sortedList !! 0) * 2 + (sortedList !! 1) * 2
+dimensions :: String -> (Int, Int, Int)
+dimensions l = do
+    let split = map read (splitOn "x" l)
+    (split !! 0, split !! 1, split !! 2) 
