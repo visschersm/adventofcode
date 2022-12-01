484dd439d559cec7b0bab938d40c25815447c5f3
Mark Visschers <visschers.m@gmail.com>
2022-12-02 00:34:07 +0800

    Added solution 03/2015 to the registration

--- Solutions/haskell/Registration.hs
+++ Solutions/haskell/Registration.hs
@@ -5,9 +5,11 @@ import qualified Data.Map as Map
 
 import Solutions.Haskell.Y2015.Solution01
 import Solutions.Haskell.Y2015.Solution02
+import Solutions.Haskell.Y2015.Solution03
 
 solutions :: Map String (String -> IO ())
 solutions = Map.fromList [
     ("y2015/Solution01", Solutions.Haskell.Y2015.Solution01.solution01), 
-    ("y2015/Solution02", Solutions.Haskell.Y2015.Solution02.solution02)
+    ("y2015/Solution02", Solutions.Haskell.Y2015.Solution02.solution02),
+    ("y2015/Solution03", Solutions.Haskell.Y2015.Solution03.solution03)
     ]
