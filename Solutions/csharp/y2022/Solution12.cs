e2ed09036a823a49adc6ca8479b72854b084928a
Mark Visschers <visschers.m@gmail.com>
2022-12-16 18:59:47 +0800

    Cleanup solution 2022/12 in csharp a bit

--- Solutions/csharp/y2022/Solution12.cs
+++ Solutions/csharp/y2022/Solution12.cs
@@ -13,46 +13,11 @@ public class Solution12
         var startPositions = new char[] { 'S' };
         var map = ReadMap(filename, startPositions);
 
-        var unvisited = map.Points.Select(point => new Node
-        {
-            Previous = null,
-            Distance = map.StartPositions.Any(sp => point.Key.x == sp.x && point.Key.y == sp.y) ? 0 : int.MaxValue,
-            Height = point.Value,
-            Point = point.Key
-        }).ToList();
-
-        List<Node> visited = new List<Node>();
-        while (unvisited.Any())
-        {
-            var current = unvisited.OrderBy(u => u.Distance).First();
-            var neighbours = unvisited.Where(u => !(u.Point.x == current.Point.x && u.Point.y == current.Point.y))
-                .Where(u => u.Height == current.Height || (u.Height - current.Height) <= 1)
-                .Where(u => Math.Abs(u.Point.x - current.Point.x) <= 1 && Math.Abs(u.Point.y - current.Point.y) <= 1)
-                .Where(u => !(Math.Abs(u.Point.x - current.Point.x) == 1 && Math.Abs(u.Point.y - current.Point.y) == 1))
-                .ToArray();
-
-            foreach (var neighbour in neighbours)
-            {
-                if (current.Distance + 1 < neighbour.Distance)
-                {
-                    neighbour.Distance = current.Distance + 1;
-                    neighbour.Previous = current;
-                }
-            }
-
-            visited.Add(current);
-            unvisited.Remove(current);
-        }
+        var unvisited = GetUnvisited(map);
+        List<Node> visited = Dijkstra(unvisited);
 
         var end = visited.Where(x => x.Point.x == map.End.x && x.Point.y == map.End.y).Single();
-
-        int steps = 0;
-        Node? node = end;
-        while (node.Previous != null)
-        {
-            steps++;
-            node = node.Previous;
-        };
+        int steps = ComputeDistance(end);
 
         // What is the fewest steps required to move from your current position to the location that should get the best signal?
         Console.WriteLine($"The fewest steps required: {steps}");
@@ -64,46 +29,11 @@ public class Solution12
         var startPositions = new []{'S', 'a'};
         var map = ReadMap(filename, startPositions);
 
-        var unvisited = map.Points.Select(point => new Node
-        {
-            Previous = null,
-            Distance = map.StartPositions.Any(sp => point.Key.x == sp.x && point.Key.y == sp.y) ? 0 : int.MaxValue,
-            Height = point.Value,
-            Point = point.Key
-        }).ToList();
-
-        List<Node> visited = new List<Node>();
-        while (unvisited.Any())
-        {
-            var current = unvisited.OrderBy(u => u.Distance).First();
-            var neighbours = unvisited.Where(u => !(u.Point.x == current.Point.x && u.Point.y == current.Point.y))
-                .Where(u => u.Height == current.Height || (u.Height - current.Height) <= 1)
-                .Where(u => Math.Abs(u.Point.x - current.Point.x) <= 1 && Math.Abs(u.Point.y - current.Point.y) <= 1)
-                .Where(u => !(Math.Abs(u.Point.x - current.Point.x) == 1 && Math.Abs(u.Point.y - current.Point.y) == 1))
-                .ToArray();
-
-            foreach (var neighbour in neighbours)
-            {
-                if (current.Distance + 1 < neighbour.Distance)
-                {
-                    neighbour.Distance = current.Distance + 1;
-                    neighbour.Previous = current;
-                }
-            }
-
-            visited.Add(current);
-            unvisited.Remove(current);
-        }
-
+        var unvisited = GetUnvisited(map);
+        List<Node> visited = Dijkstra(unvisited);
+        
         var end = visited.Where(x => x.Point.x == map.End.x && x.Point.y == map.End.y).Single();
-
-        int steps = 0;
-        Node? node = end;
-        while (node.Previous != null)
-        {
-            steps++;
-            node = node.Previous;
-        };
+        int steps = ComputeDistance(end);
 
         // What is the fewest steps required to move from your current position to the location that should get the best signal?
         Console.WriteLine($"The fewest steps required: {steps}");
@@ -177,5 +107,58 @@ public class Solution12
         public Point[] StartPositions;
         public Point End;
         public Dictionary<Point, char> Points = new Dictionary<Point, char>();
+    }
+
+    public List<Node> GetUnvisited(Map map)
+    {
+        return map.Points.Select(point => new Node
+        {
+            Previous = null,
+            Distance = map.StartPositions.Any(sp => point.Key.x == sp.x && point.Key.y == sp.y) ? 0 : int.MaxValue,
+            Height = point.Value,
+            Point = point.Key
+        }).ToList();
+    }
+
+    public List<Node> Dijkstra(List<Node> unvisited)
+    {
+        var visited = new List<Node>();
+        
+        while (unvisited.Any())
+        {
+            var current = unvisited.OrderBy(u => u.Distance).First();
+            var neighbours = unvisited.Where(u => !(u.Point.x == current.Point.x && u.Point.y == current.Point.y))
+                .Where(u => u.Height == current.Height || (u.Height - current.Height) <= 1)
+                .Where(u => Math.Abs(u.Point.x - current.Point.x) <= 1 && Math.Abs(u.Point.y - current.Point.y) <= 1)
+                .Where(u => !(Math.Abs(u.Point.x - current.Point.x) == 1 && Math.Abs(u.Point.y - current.Point.y) == 1))
+                .ToArray();
+
+            foreach (var neighbour in neighbours)
+            {
+                if (current.Distance + 1 < neighbour.Distance)
+                {
+                    neighbour.Distance = current.Distance + 1;
+                    neighbour.Previous = current;
+                }
+            }
+
+            visited.Add(current);
+            unvisited.Remove(current);
+        }
+
+        return visited;
+    }
+
+    public int ComputeDistance(Node node)
+    {
+        int steps = 0;
+        
+        while (node.Previous != null)
+        {
+            steps++;
+            node = node.Previous;
+        };
+
+        return steps;
     }
 }
