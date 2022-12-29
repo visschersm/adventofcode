589379a5db7188553317c9546c007b6f8a7f1d33
Mark Visschers <visschers.m@gmail.com>
2022-12-30 00:53:14 +0100

    Added solution for 2015/03 part1 in python

---
+++ Solutions/python/y2015/Solution03.py
@@ -0,0 +1,40 @@
+from util.filereader import *
+
+
+def move(position, c):
+   x, y = position
+   match c:
+      case "^":
+         return (x, y + 1)
+      case ">":
+         return (x + 1, y)
+      case "v":
+         return (x, y - 1)
+      case "<":
+         return (x - 1, y)
+
+
+def part1(input_file):
+   result = []
+   previousPosition = (0, 0)
+   result.append(previousPosition)
+
+   for c in read_by_character(input_file):
+      previousPosition = move(previousPosition, c)
+      result.append(previousPosition)
+
+   count = len(set(result))
+   print("Number of houses that receive a present: " + str(count))
+
+
+def part2(input_file):
+   result = []
+   previousPosition = (0, 0)
+   result.append(previousPosition)
+
+   for c in read_by_character(input_file):
+      previousPosition = move(previousPosition, c)
+      result.append(previousPosition)
+
+   count = len(set(result))
+   print("Number of houses that receive a present: " + str(count))
