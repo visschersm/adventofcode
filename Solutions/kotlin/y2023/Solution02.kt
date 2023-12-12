3377490078fa30a4f5091bf21e455fb5511221b3
Mark Visschers <visschers.m@gmail.com>
2023-12-13 00:57:43 +0100

    optimized 2023/02 part1 in kotlin

--- Solutions/kotlin/y2023/Solution02.kt
+++ Solutions/kotlin/y2023/Solution02.kt
@@ -8,66 +8,27 @@ import java.io.File
 
 class Solution02 : Solution {
     override fun Part1(inputFile: String) {
-        val maxRedValue = 12
-        val maxGreenValue = 13
-        val maxBlueValue = 14
+        val maxValues = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
         // Game 1: 4 red, 18 green, 15 blue; 17 green, 18 blue, 9 red; 8 red, 14 green, 6 blue; 14
         // green, 12 blue, 2 red
         val result =
-                File(inputFile)
-                        .readLines()
-                        .filter() { line ->
-                            val dices: String = line.split(":")[1]
-                            dices.split(";").all() { dice ->
-                                dice.split(",").all() { die ->
-                                    // Define a regular expression pattern to match numbers
-                                    val regexPattern = Regex("\\d+")
-
-                                    // Find the first match in the string
-                                    val matchResult = regexPattern.find(die)
-
-                                    // Extract the matched number as a string
-                                    val matchedNumberString: String? = matchResult?.value
-
-                                    if (matchedNumberString == null) {
-                                        false
-                                    } else {
-                                        // Convert the string to an actual number (if needed)
-                                        val matchedNumber: Int = matchedNumberString.toInt()
-
-                                        if (die.contains("red") && matchedNumber > maxRedValue) {
-                                            false
-                                        } else if (die.contains("green") &&
-                                                        matchedNumber > maxGreenValue
-                                        ) {
-                                            false
-                                        } else if (die.contains("blue") &&
-                                                        matchedNumber > maxBlueValue
-                                        ) {
-                                            false
-                                        } else {
-                                            true
-                                        }
-                                    }
-                                }
+                File(inputFile).readLines().sumOf() { line ->
+                    var gameNumberRegexPattern = Regex("Game (?<game>\\d+):")
+                    val gameNumberMatch = gameNumberRegexPattern.find(line) ?: throw Exception()
+                    val matchNumber =
+                            gameNumberMatch.groups["game"]?.value?.toInt() ?: throw Exception()
+                    val regexPattern = Regex("(?<count>\\d+) (?<color>\\b\\w+\\b)(,|;)?")
+                    var matches = regexPattern.findAll(line)
+                    val possible =
+                            matches.all { match ->
+                                val count =
+                                        match.groups["count"]?.value?.toInt() ?: throw Exception()
+                                val color = match.groups["color"]?.value ?: throw Exception()
+                                maxValues[color]!! > count
                             }
-                        }
-                        .sumOf() { game ->
-                            val gameNumber: String = game.split(":")[0]
-                            // Define a regular expression pattern to match numbers
-                            val regexPattern = Regex("\\d+")
-
-                            // Find the first match in the string
-                            val matchResult = regexPattern.find(gameNumber)
-
-                            // Extract the matched number as a string
-                            val matchedNumberString: String? = matchResult?.value
-
-                            val matchedNumber: Int = matchedNumberString?.toInt() ?: 0
-
-                            matchedNumber
-                        }
+                    if (possible) matchNumber else 0
+                }
 
         println("Result: $result")
     }
